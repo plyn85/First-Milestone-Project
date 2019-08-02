d5889ced9c345c286a59012b0c1bcd21f7e5f216
plyn85 <plyn99@gmail.com>
2019-08-02 16:37:26 +0100

    adding credit for code used

--- Assets/js/fancyfilename.js
+++ Assets/js/fancyfilename.js
@@ -1,3 +1,13 @@
+//gym  section
+
+// gallery sub-section
+
+//taken from from a tutorial found at https://tutorialzine.com/2018/03/3-amazing-bootstrap-4-gallery-templates f
+
 baguetteBox.run('.compact-gallery', {
     animation: 'slideIn'
 });
+
+// end gym section
+
+// end  gallery sub-section
