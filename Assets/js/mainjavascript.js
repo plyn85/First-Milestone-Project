3e0af26c30931e9c765885c89fe6ff9aa579691a
plyn85 <plyn99@gmail.com>
2019-08-13 16:23:16 +0100

    renamed javascript file

---
+++ Assets/js/mainjavascript.js
@@ -0,0 +1,9 @@
+
+//taken from from a tutorial found at https://tutorialzine.com/2018/03/3-amazing-bootstrap-4-gallery-templates f
+
+baguetteBox.run('.compact-gallery', {
+    animation: 'slideIn'
+});
+
+
+
