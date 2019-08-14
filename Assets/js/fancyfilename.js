7368e159fb97c9cf666484b1c6a2cf7c7a6165ea
plyn85 <plyn99@gmail.com>
2019-08-14 10:59:22 +0100

    changed image file name

--- Assets/js/fancyfilename.js
+++
@@ -1,9 +0,0 @@
-
-//taken from from a tutorial found at https://tutorialzine.com/2018/03/3-amazing-bootstrap-4-gallery-templates f
-
-baguetteBox.run('.compact-gallery', {
-    animation: 'slideIn'
-});
-
-
-
